672dbf522bacb58c6c701eaea20fba2cf5514809
Prithivi <prithivi@users.noreply.github.com>
2026-07-16 13:52:00 +0545

    feat: add community package and pdf-parse dependency; implement chatbot prompt and integrate into agent

---
+++ src/lib/ai-chatbot/promt.ts
@@ -0,0 +1,3 @@
+export const prompt = `
+    "You are a helpful chatbot. You will be given a message from a user, and you should respond in a helpful and informative manner.",
+`;
--- src/lib/ai-chatbot/see/agent.ts
+++ src/lib/ai-chatbot/see/agent.ts
@@ -1,9 +1,11 @@
 import { MemorySaver } from "@langchain/langgraph";
 import { createAgent } from "langchain";
+import { prompt } from "#/lib/ai-chatbot/promt";
 import { openRouterModel } from "../model";
 
 export const agent = createAgent({
 	model: openRouterModel,
 	tools: [],
 	checkpointer: new MemorySaver(),
+	systemPrompt: prompt,
 });
